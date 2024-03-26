f839ef69c553a1deb539ec3120970c0c1c1aa790
uczen <uczen@email.pl>
2024-03-26 12:06:45 +0100

    Dodaj pliki projektów.

---
+++ PONG_Sroka/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PONG_Sroka
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
