16b590172364951de99a9e648c51eaf86eac324c
PiotrSroka2005 <piotr09sroka@interia.pl>
2024-03-26 12:11:39 +0100

    Add ICanvasObject interface

---
+++ PONG_Sroka/ICanvasObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace PONG_Sroka
+{
+    public interface ICanvasObject
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Canvas Canvas { get; set; }
+        public void Draw();
+        public void Reset();
+    }
+}
