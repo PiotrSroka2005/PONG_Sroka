0cc3b768da08ed9d826b3e3aaba1ed19804f8ea9
PiotrSroka2005 <piotr09sroka@interia.pl>
2024-03-26 12:16:27 +0100

    Add constructor and functions to change behaviour of ball in Ball class

--- PONG_Sroka/Ball.cs
+++ PONG_Sroka/Ball.cs
@@ -22,6 +22,51 @@ namespace PONG_Sroka
         public double Speed { get; set; }
         public Ellipse Shape { get; set; }
 
-        
+        public Ball(int width, int height, Canvas canvas)
+        {
+            Width = width;
+            Height = height;
+            Canvas = canvas;
+            Shape = new()
+            {
+                Width = width,
+                Height = height,
+                Fill = new SolidColorBrush(Colors.White)
+            };
+            Reset();
+            Canvas.Children.Add(Shape);
+        }
+
+        public void Draw()
+        {
+            Canvas.SetLeft(Shape, X);
+            Canvas.SetTop(Shape, Y);
+        }
+        public void Move()
+        {
+            X += Math.Cos(Angle) * Speed * DirectionX;
+            Y += Math.Sin(Angle) * Speed * DirectionY;
+
+            if (Y - Height <= 0 || Y + Height >= Canvas.Height)
+            {
+                DirectionY *= -1;
+                Speed += 0.5f;
+            }
+
+            Draw();
+        }
+        public void Reset()
+        {
+            X = Canvas.Width / 2 - Width / 2;
+            Y = Canvas.Height / 2 - Height / 2;
+            Random random = new();
+            Speed = 3f;
+            DirectionX = random.Next(1, 30) % 2 == 0 ? 1 : -1;
+            DirectionY = random.Next(1, 30) % 2 == 0 ? 1 : -1;
+            Random r = new();
+            Angle = r.Next(30, 60) * Math.PI / 180;
+        }
+
+
     }
 }
