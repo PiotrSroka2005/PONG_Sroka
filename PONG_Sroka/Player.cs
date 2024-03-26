c89c547089a3b15fb274b1b708cbacaf58f0bac6
PiotrSroka2005 <piotr09sroka@interia.pl>
2024-03-26 12:19:07 +0100

    Add Player class which  inherit from ICanvasObject and it;s properties

---
+++ PONG_Sroka/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace PONG_Sroka
+{
+    public class Player : ICanvasObject
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Canvas Canvas { get; set; }
+        public Rectangle Shape { get; set; }
+        public bool MousePlayer { get; set; }
+        public int Points { get; set; }
+        
+    }
+}
